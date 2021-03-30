a1542dc22ad1dcd42fbafe9ed30bea3031510e68
VictorManuelTamayo <victortamayo509@gmail.com>
2021-03-29 23:37:20 -0600

    new-style
    nuevo estilo en el formulario de registro de empleados

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -12,18 +12,12 @@ class _InputPageState extends State<InputPage> {
   String _email = '';
   String _password = '';
   String _fecha = '';
-  List<String> _lenguajes = [
-    'programador',
-    'administrador',
-    'consultordb',
-    'otro',
-  ];
-  String _opcionSelecciona = 'programador';
+
   TextEditingController _inputFieldDateController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text('Entradas de texto'),
@@ -31,149 +25,117 @@ class _InputPageState extends State<InputPage> {
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
-          _crearEntrada(),
+          _crearNombre(),
           Divider(),
-          _crearEmail(),
+          _crearApellidos(),
           Divider(),
-          _crearPassword(),
+          _crearRfc(),
           Divider(),
-          _crearFecha(context),
-          Divider(),
-          _crearDropDown(),
-          Divider(),
-          _crearPersona(),
+          _crearNumeroEmpleado(),
         ],
       ),
     );
   }
 
-  Widget _crearEntrada() {
-    return TextField(
-      autofocus: true,
-      decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-          counter: Text('Caracteres ${_nombre.length}'),
-          hintText: 'Nombre de la persona',
-          labelText: 'Nombre',
-          suffixIcon: Icon(Icons.accessibility),
-          icon: Icon(Icons.account_circle)),
-      onChanged: (valor) {
-        setState(() {
-          _nombre = valor;
-          //print(_nombre);
-        });
-      },
-    );
-  }
-
-  Widget _crearPersona() {
-    return ListTile(
-      title: Text('El nombre es: $_nombre'),
-      subtitle: Text('Email: $_email, Password: $_password'),
-      trailing: Text('area: $_opcionSelecciona'),
-    );
-  }
-
-  Widget _crearEmail() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-          hintText: 'Email de la persona',
-          labelText: 'Email',
-          helperText: 'Escriba su email',
-          suffixIcon: Icon(Icons.alternate_email),
-          icon: Icon(Icons.email)),
-      onChanged: (valor) {
-        setState(() {
-          _email = valor;
-          //print(_nombre);
-        });
-      },
-    );
-  }
-
-  Widget _crearPassword() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-          hintText: 'Password',
-          labelText: 'Password',
-          helperText: 'Escriba su password',
-          suffixIcon: Icon(Icons.lock_open),
-          icon: Icon(Icons.lock)),
-      onChanged: (valor) {
-        setState(() {
-          _email = valor;
-          //print(_nombre);
-        });
-      },
-    );
-  }
-
-  Widget _crearFecha(BuildContext context) {
-    return TextField(
-      enableInteractiveSelection: false,
-      controller: _inputFieldDateController,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-        hintText: 'Fecha de entrada',
-        labelText: 'Fecha del dia de hoy',
-        helperText: 'Ingrese hora y entrada',
-        suffixIcon: Icon(Icons.perm_contact_calendar),
-        icon: Icon(Icons.calendar_today),
+  Widget _crearNombre() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical:10, horizontal:10),
+      decoration: BoxDecoration(
+        color: Colors.amber[300].withOpacity(0.5),
+        borderRadius: BorderRadius.circular(16),
       ),
-      onTap: () {
-        FocusScope.of(context).requestFocus(new FocusNode());
-        _selectDate(context);
-      },
+      child:TextField(
+        autofocus: true,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            //counter: Text('Caracteres ${_nombre.length}'),
+            hintText: 'Nombre',
+            //labelText: 'Nombre',
+            icon: Icon(Icons.account_circle,  color: Colors.amber)),
+        onChanged: (valor) {
+          setState(() {
+            _nombre = valor;
+            //print(_nombre);
+          });
+        },
+      )
     );
   }
 
-  _selectDate(BuildContext context) async {
-    DateTime picked = await showDatePicker(
-      context: context,
-      initialDate: new DateTime.now(),
-      firstDate: new DateTime(2020),
-      lastDate: new DateTime(2022),
-      locale: Locale('es', 'ES'),
-    );
-    if (picked != null) {
-      setState(() {
-        _fecha = picked.toString();
-        _inputFieldDateController.text = _fecha;
-      });
-    }
-  }
-
-  Widget _crearDropDown() {
-    return Row(
-      children: <Widget>[
-        Icon(Icons.code),
-        SizedBox(width: 30.0),
-        Text('Area del personal '),
-        Expanded(
-          child: DropdownButton(
-            value: _opcionSelecciona,
-            items: getOpcionesDropDown(),
-            onChanged: (opt) {
-              setState(() {
-                _opcionSelecciona = opt;
-              });
-              //print(opt);
-            },
-          ),
-        ),
-      ],
+  Widget _crearApellidos() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical:10, horizontal:10),
+      decoration: BoxDecoration(
+        color: Colors.amber[300].withOpacity(0.5),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child:TextField(
+        autofocus: true,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            //counter: Text('Caracteres ${_nombre.length}'),
+            hintText: 'Apellidos',
+            //labelText: 'Nombre',
+            icon: Icon(Icons.account_circle,  color: Colors.amber)),
+        onChanged: (valor) {
+          setState(() {
+            _nombre = valor;
+            //print(_nombre);
+          });
+        },
+      )
     );
   }
 
-  List<DropdownMenuItem<String>> getOpcionesDropDown() {
-    List<DropdownMenuItem<String>> lista = new List();
-    _lenguajes.forEach((lenguaje) {
-      lista.add(DropdownMenuItem(child: Text(lenguaje), value: lenguaje));
-    });
-    return lista;
+  Widget _crearRfc() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical:10, horizontal:10),
+      decoration: BoxDecoration(
+        color: Colors.amber[300].withOpacity(0.5),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child:TextField(
+        autofocus: true,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            //counter: Text('Caracteres ${_nombre.length}'),
+            hintText: 'RFC',
+            //labelText: 'Nombre',
+            icon: Icon(Icons.account_circle,  color: Colors.amber)),
+        onChanged: (valor) {
+          setState(() {
+            _nombre = valor;
+            //print(_nombre);
+          });
+        },
+      )
+    );
   }
+
+  Widget _crearNumeroEmpleado() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical:10, horizontal:10),
+      decoration: BoxDecoration(
+        color: Colors.amber[300].withOpacity(0.5),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child:TextField(
+        autofocus: true,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            //counter: Text('Caracteres ${_nombre.length}'),
+            hintText: 'Numero de empleado: 223',
+            //labelText: 'Nombre',
+            icon: Icon(Icons.account_circle,  color: Colors.amber)),
+        onChanged: (valor) {
+          setState(() {
+            _nombre = valor;
+            //print(_nombre);
+          });
+        },
+      )
+    );
+  }
+
+  
 }
